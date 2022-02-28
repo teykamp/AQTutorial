3f3a3058427fe130d2deb482b19b068fd60a98dc
eykamt <embergameslp@gmail.com>
2022-02-28 01:34:48 -0500

    Functional Site

--- aqtutorial/src/App.vue
+++ aqtutorial/src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <!-- Rotate -->
+  <div v-show="!itemArr[itemNum].includes('Text') && !itemArr[itemNum].includes('CombinedFilter') && !itemArr[itemNum].includes('Tape')" class="button-container">
+    <button @click="$refs.tutorial.moveImage(false)">Left</button>
+    <button @click="$refs.tutorial.moveImage(true)">Right</button>
   </div>
+  <h1>{{ isError ? "ERROR":"" }}</h1>
+  <h1 class="instructions">Instructions go here!</h1>
+  <!-- Continue -->
+  <button v-show="itemNum < itemArr.length-1" @click.left="check()">Continue</button>
+  <center>
+    <div >
+      <TutorialView ref="tutorial" :item="itemArr[itemNum]" />
+    </div>
+  </center>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TutorialView from './components/TutorialView.vue'
 
 export default {
   name: 'App',
+  data: () => {
+    return {
+      itemNum: 0,
+      itemArr: ['Text1' , 'Fan', 'Filter', 'Tape', 'CombinedFilter', 'Text2'],
+      isError: false,
+    }
+  },
   components: {
-    HelloWorld
+    TutorialView
+  },
+  methods: {
+    check() {
+      this.isError = !this.$refs.tutorial.check();
+      if (!this.isError) {
+        this.itemNum++
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button, img {
+  user-select: none;
+}
+div.button-container {
+  margin: 2.5%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 1vw;
+  background:rgba(159, 178, 197, 0.671);
+  border-radius: 10px;
+}
+button {
+  height: 5vh;
+  width: 5vw;
+  background-color: white;
+  border: 2px solid black;
+  font-weight: bold;
+  font-size: 1rem;
+  border-radius: 10px;
+  margin: 10px;
+}
+button:hover {
+  background-color: rgb(175, 191, 241);
+  box-shadow: 1px 1px 2px;
 }
 </style>
---
+++ aqtutorial/src/components/TutorialView.vue
@@ -0,0 +1,73 @@
+<template>
+<div>
+  <div class="master">
+        
+        <img v-if="!item.includes('Text')" :src="require(`../assets/${item}Img/${item}${imgNumber % 24}.png`)" alt="test image" />
+
+        <div v-else-if="item === 'Text1'">
+          <pre>
+            Hi this is text1
+          </pre>
+        </div>
+
+        <div v-else>
+          <pre>
+          this is text 2
+          </pre>
+        </div>  
+    
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  data: () => {
+    return {
+      imgNumber: 13, // 13 default because that is correct rotation direction
+    }
+  },
+  props: [
+    'item',
+  ],
+  methods: {
+    moveImage(direction) {
+      direction ? this.imgNumber--:this.imgNumber++;
+    },
+    check() {
+      // error check
+      return this.imgNumber % 24 === 13;
+    }
+  },
+  watch: {
+    item() {
+      if (this.item === 'Tape' || this.item === 'CombinedFilter') {
+        this.imgNumber = 13;
+      } else {
+        this.imgNumber = (Math.round(Math.random() * 24) + 1);
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  max-height: 70vh;
+  width: auto;
+  object-fit: cover;
+}
+div.master {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+pre {
+  white-space: pre-wrap;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 2ch;
+}
+</style>
+
